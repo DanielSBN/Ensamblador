959efebb14b32b0eb8d84d7fb575c96cda20878e
Daniel Sanchez Buitrago <dansanchezbui@unal.edu.co>
2017-11-02 17:39:11 -0500

    Corrección de Lectura de Datos

--- main.asm
+++ main.asm
@@ -1,5 +1,5 @@
 ; PROYECTO DE LENGUAJE ENSAMBLADOR
-; ARQUITECTURA DE COMPUTADORES, SEMESTRE 2017 - II
+; ARQUITECTURA DE COMPUTADORES, SEMESTRE 2017-II
 ; Integrantes: Kewin Evers Yagari, Daniel Sanchez Buitrago, Daniel Velez Santamaria
 ; Docente: Hugo de Jesus Mesa Yepes
 
@@ -14,47 +14,24 @@ INCLUDE Irvine32.inc
 	nodos BYTE "Ingrese la cantidad de nodos del grafo: ", 0
 
 	; Mensaje para pedir las conexiones en el grafo
-	con BYTE "Ingrese las distancias del nodo ", 0
-	con2 BYTE " al resto de los nodos (si no estan conectados, digite un 0): ", 0
+	con1 BYTE "Ingrese la distancia del nodo ", 0
+	con2 BYTE " al nodo ", 0
+	con3 BYTE ": ", 0
 
 	; Datos auxiliares para obtener las conexiones del grafo
-	aux BYTE 255 DUP(?)
-	siz DWORD ?
-	num BYTE 255 DUP(?)
+	partida BYTE ?
+	destino BYTE ?
 
 	; Cantidad de nodos del grafo
 	n DWORD ?
-
-	; Matriz de adyacencia del grafo
-	grafo DWORD ?
+	
+	; Cantidad total de elementos de la matriz de adyacencia
+	dim DWORD ?
+	
+	; Numero total de bytes que ocupa la matriz de adyacencia
+	siz DWORD ?
 
 .CODE
-	; Procedimiento para separar la cadena de caracteres con las conexiones de un nodo de tal manera que se obtenga cada longitud por separada
-	CutString PROC, chain:PTR BYTE, tam:DWORD
-		; Ingresamos ecx y eax a la pila para preservar su valor
-		push ecx
-		push eax
-
-		mov esi, chain
-		mov ecx, 0
-		mov al, ' '
-wc:		cmp ecx, tam
-		jge ewc
-			cmp [esi], eax
-			jne outc
-				push 0
-				pop [esi]
-outc:		add esi, TYPE BYTE
-			inc ecx
-			jmp wc
-ewc:
-		; Extraemos los valores originales de ecx y eax de la pila
-		pop eax
-		pop ecx
-
-		ret
-	CutString ENDP
-	
 	main PROC
 		; Imprimimos el mensaje de bienvenida
 		mov edx, OFFSET bienvenida
@@ -68,47 +45,39 @@ ewc:
 		call ReadDec
 		mov n, eax
 		
-		mov ebx, 1 ; Usamos ebx como contador para pedir las conexiones
-		mov esi, OFFSET con
+		; Inicializamos las variables dim y siz
+		mov eax, n
+		imul eax, n
+		mov dim, eax
+		imul eax, TYPE DWORD
+		mov siz, eax
 		
-		; Para ebx desde 1 hasta n, obtenga las conexiones de los nodos
-whc:	cmp ebx, n
-		jg ewhc
-			; Pedimos las conexiones
-			mov edx, OFFSET con
-			call WriteString
-			
-			add ebx, '0' ; Convertimos el numero a ASCII
-			mov eax, ebx ; Movemos el numero del nodo actual al mensaje
-			call WriteChar
-			
-			mov edx, OFFSET con2
-			call WriteString
-			
-			mov edx, OFFSET aux
-			mov ecx, SIZEOF aux
-			call ReadString
-			mov siz, eax
-			
-			; Procesamos las conexiones y las guardamos en la matriz de adyacencia
-			invoke CutString, ADDR aux, LENGTHOF aux
-			
-			mov esi, 0
-			mov ecx, 0
-w1:			cmp esi, LENGTHOF aux
-			jge ew1
-				movzx eax, aux[esi]
-				call WriteString
-				inc esi
-				jmp w1
-ew1:
-			
-			sub ebx, '0'
-			inc ebx
-			jmp whc
-ewhc:
+		; Inicializamos la matriz de adyacencia en la pila como un arreglo
+		mov eax, siz
+		push esp ; Introducimos el valor de esp a la pila para conservarlo al final del programa
+		sub esp, eax ; Ahora, esp apunta al elemento inicial del arreglo
 		
+		; PEDIMOS LAS CONEXIONES DEL GRAFO
+		
+		mov ebx, 0 ; Iterador para filas
+		mov ecx, 0 ; Iterador para columnas
+		
+		; Ejecutamos un ciclo que recorre la matriz de adyacencia, pidiendo la distancia correspondiente desde el nodo referenciado por ebx
+		; al nodo referenciado por ecx
+wFila:	cmp ebx, n ; Mientras ebx sea menor a n, iteramos (iteracion de filas)
+		jge ewF
+wCol:		cmp ecx, n ; Mientras ecx sea menor a n, iteramos (iteracion de columnas)
+			jge ewC
+				cmp ebx, ecx ; Si ebx y ecx son iguales, saltamos a la siguiente iteracion
+				je siFCeq
+					
+siFCeq:			inc ecx
+				jmp wCol
+ewC:		inc ebx
+			jmp wFila
+ewF:
+		
+		pop esp
 		exit
 	main ENDP
-
 	END main
